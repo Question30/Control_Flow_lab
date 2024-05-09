1a9cca5b6849cea7f58581b8a43f3cb0c19351b3
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-05-09 15:11:13 -0400

    Part 1 completed

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Test");
+//Part 1
+
+//Variables
+const PI = 3.1415;
+const radius = 5;
+const area = PI * radius * radius;
+const plantMinSpace = 0.8;
+const week1Plants = 20;
+const week2Plants = 40;
+const week3Plants = 80;
+
+//Plantarea
+const week1Area = plantMinSpace * week1Plants;
+const week2Area = plantMinSpace * week2Plants;
+const week3Area = plantMinSpace * week3Plants;
+
+//Percentage of total space
+const week1Percentage = (week1Area / area) * 100;
+const week2Percentage = (week2Area / area) * 100;
+const week3Percentage = (week3Area / area) * 100;
+
+function PlantStatus(percentage) {
+  if (percentage > 80) {
+    console.log("Pruned");
+  } else if (percentage < 80 && percentage >= 50) {
+    console.log("Monitored");
+  } else {
+    console.log("Planted");
+  }
+}
+
+PlantStatus(week1Percentage);
+PlantStatus(week2Percentage);
+PlantStatus(week3Percentage);
